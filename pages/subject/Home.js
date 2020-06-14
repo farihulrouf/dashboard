fef41055de140c21ed494c7b2c193d7dceb43f38
Ramandika <theone_rama@yahoo.com>
2020-06-14 20:50:40 +0700

    add some design to home.js

--- pages/subject/Home.js
+++ pages/subject/Home.js
@@ -1,10 +1,14 @@
 import NavBar from '../../components/NavBar.js';
 import { makeStyles } from '@material-ui/core/styles';;
-import {Grid, List, ListItem, ListItemAvatar, Avatar, ListItemText,
-    Typography, Paper, Button, IconButton, InputBase, Divider
+import {Grid, List, ListItem, ListItemIcon, Checkbox, ListItemSecondaryAction, Avatar, ListItemText,
+    Typography, Paper, Button, IconButton, InputBase, Divider, TextareaAutosize
 } from '@material-ui/core';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import SendIcon from '@material-ui/icons/Send';
 import CommentIcon from '@material-ui/icons/Comment';
+import AnnouncementIcon from '@material-ui/icons/AnnouncementOutlined';
+import ExamIcon from '@material-ui/icons/AssessmentOutlined'
+import ClassIcon from '@material-ui/icons/ClassOutlined'
 import ShareIcon from '@material-ui/icons/Share';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
@@ -31,67 +35,103 @@ const useStyles = makeStyles(styles);
 const Item = (props) => {
     const classes = useStyles();
     const {data} = props
+    const [showComment,setShowCommnet] = React.useState(false);
+
+    const showCommentBox = (event) => {
+       setShowCommnet(!showComment);
+    }
+
     return(
-    <Paper elevation={3} style={{marginBottom: 20}}>
-        <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-                <Avatar alt={data.sender} src="/static/images/avatar/1.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-            primary={data.title}
-            secondary={
-                <React.Fragment>
-                    <Typography
-                        component="span"
-                        variant="body2"
-                        className={classes.inline}
-                        color="textPrimary"
-                    >
-                        {`${data.sender} | ${data.date}`}
-                    </Typography>
+        <Paper elevation={3} style={{marginBottom: 30, padding: 20}}>
+            <Grid container spacing={2}>
+                <Grid item>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </Grid>
+                <Grid item>
+                    <Grid container><b>{data.title}</b></Grid>
                     <Grid container>
-                        <p>{data.body}</p>
+                        <React.Fragment>
+                        <Typography
+                            component="span"
+                            variant="body2"
+                            className={classes.inline}
+                            color="textPrimary"
+                        >
+                            {`${data.sender} - ${data.date}`}
+                        </Typography>
+                        </React.Fragment>
                     </Grid>
-                    <Grid container spacing={3}>
-                        <Grid item>
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                size="small"
-                                className={classes.button}
-                                startIcon={<ThumbUpIcon />}
-                            >
-                                Like
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                size="small"
-                                className={classes.button}
-                                startIcon={<CommentIcon />}
-                            >
-                                Comment
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                size="small"
-                                className={classes.button}
-                                startIcon={<ShareIcon />}
-                            >
-                                Share
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </React.Fragment>
-            }
-        />
-    </ListItem>
-    </Paper>
+                </Grid>
+            </Grid>
+            <Grid container style={{marginTop: 10}}>
+                <ListItemText primary={data.body} />
+            </Grid>
+            <Grid container style={{marginTop: 10}}>
+                {/* <Button 
+                    startIcon={<ThumbUpIcon style={{fontSize: 15}} />} color="primary" 
+                    variant="contained"
+                    style={{padding: 0, height: 20}}
+                />  */}
+                    <ThumbUpIcon style={{fontSize: 15, color:"#556cd6", marginRight: 10}} />
+                    <span style={{fontSize: 12}}>232 Likes</span>
+            </Grid>
+            <Grid container spacing={3} style={{marginTop: 10}}>
+                <Grid item>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        size="small"
+                        className={classes.button}
+                        startIcon={<ThumbUpIcon />}
+                    >
+                        Like
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        size="small"
+                        className={classes.button}
+                        startIcon={<CommentIcon />}
+                        onClick={showCommentBox}
+                    >
+                        Comment
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        size="small"
+                        className={classes.button}
+                        startIcon={<ShareIcon />}
+                    >
+                        Share
+                    </Button>
+                </Grid>
+            </Grid>
+            <Divider style={{marginTop: 10}}/>
+            {showComment && <Grid container style={{marginTop: 10}}>
+                <Grid xs={12} sm={11} item>
+                    <TextareaAutosize
+                        placeholder="Write your comments"
+                        multiline="true"
+                        rowsMin={2}
+                        rowsMax={10}
+                        style={{width: '100%', borderRadius: 10}}
+                    />
+                </Grid>
+                <Grid xs={12} sm={1} item>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                    >
+                        <SendIcon style={{fontSize: 15}} />
+                    </Button>
+                </Grid>
+            </Grid>}
+        </Paper>
     )
 }
 
@@ -111,61 +151,110 @@ const SearchBar = () => {
     )
 }
 
-const AnnouncementBoard = () => {
-    return(
-        <Paper elevation={3} style={{paddingLeft: 20, marginTop: 10}}>
-                <h3>Papan Pengumuman</h3>
-        </Paper>
-    )
+const TagFilter = () => {
+    const classes = useStyles();
+    const [checked, setChecked] = React.useState([]);
+  
+    const handleToggle = (value) => () => {
+      const currentIndex = checked.indexOf(value);
+      const newChecked = [...checked];
+  
+      if (currentIndex === -1) {
+        newChecked.push(value);
+      } else {
+        newChecked.splice(currentIndex, 1);
+      }
+  
+      setChecked(newChecked);
+    };
+
+    const filters = [
+        {id: 1, name: 'Berita',icon: <AnnouncementIcon />},
+        {id: 2, name: 'Kelas', icon: <ClassIcon />},
+        {id: 3, name: 'Ujian', icon: <ExamIcon />}
+    ]
+    return (
+      <List>
+        {filters.map((value) => {
+          const labelId = `checkbox-list-label-${value.id}`;
+  
+          return (
+            <ListItem key={value.id} role={undefined} dense button onClick={handleToggle(value.id)}>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(value.id) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={value.name} />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="comments">
+                  {value.icon}
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+      </List>
+    );
 }
 
 const Home = () => {
     const classes = useStyles();
     const data = [
         {
+            id: 1,
             title: 'Ujian Kalkulus diundur',
             body: `Dikarenakan satu dan lain hal ujian kalkulus IA akan diundur 
             menjadi tanggal 20 Agustus 2020`,
             sender: 'Yudistira',
-            date: new Date()
+            date: '3 September 2020, 15:20'
         },
         {
+            id: 2,
             title: 'Tugas Besar II',
             body: `Tugas besar dua dirilis perhari ini, silahkan cek dokumen terlampir
             untuk detail spesifikasi tugas besar II. Deadline tubes II ini 2 minggu dari
             hari ini`,
             sender: 'Agus Yodi',
-            date: new Date()
+            date: '3 September 2020, 15:20'
         },
         {
+            id: 3,
             title: 'Ujian Kalkulus diundur',
             body: `Dikarenakan satu dan lain hal ujian kalkulus IA akan diundur 
             menjadi tanggal 20 Agustus 2020`,
             sender: 'Yudistira',
-            date: new Date()
+            date: '3 September 2020, 15:20'
         },
         {
+            id: 4,
             title: 'Tugas Besar II',
             body: `Tugas besar dua dirilis perhari ini, silahkan cek dokumen terlampir
             untuk detail spesifikasi tugas besar II. Deadline tubes II ini 2 minggu dari
             hari ini`,
             sender: 'Agus Yodi',
-            date: new Date
+            date: '3 September 2020, 15:20'
         },
         {
+            id: 5,
             title: 'Ujian Kalkulus diundur',
             body: `Dikarenakan satu dan lain hal ujian kalkulus IA akan diundur 
             menjadi tanggal 20 Agustus 2020`,
             sender: 'Yudistira',
-            date: new Date()
+            date: '3 September 2020, 15:20'
         },
         {
+            id: 6,
             title: 'Tugas Besar II',
             body: `Tugas besar dua dirilis perhari ini, silahkan cek dokumen terlampir
             untuk detail spesifikasi tugas besar II. Deadline tubes II ini 2 minggu dari
             hari ini`,
             sender: 'Agus Yodi',
-            date: new Date()
+            date: '3 September 2020, 15:20'
         }
     ]
 
@@ -174,12 +263,12 @@ const Home = () => {
             <Grid container>
                 <Grid item xs={12} sm={3}>
                     <SearchBar />
-                    <AnnouncementBoard />
+                    <TagFilter />
                 </Grid>
                 <Grid item xs={12} sm={9}>
                     <Grid container style={{justifyContent: 'center'}}>
                         <List className={classes.root}>
-                            {data.map((value) => <Item data={value} />)}
+                            {data.map((value) => <Item key={value.id} data={value} />)}
                         </List>
                     </Grid>
                 </Grid>
