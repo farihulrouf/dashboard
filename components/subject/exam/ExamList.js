00f5f6d20aa327b4363ed6ead5485187eb6f633d
Shalahudin Al Ayyub <ayubalandroid@gmail.com>
2021-02-20 17:01:58 +0700

    Update exam item style

--- components/subject/exam/ExamList.js
+++ components/subject/exam/ExamList.js
@@ -5,19 +5,13 @@ import AddIcon from '@material-ui/icons/Add';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 class ExamList extends React.Component{
-    ITEM_HEIGHT = 48;
-
     state = {
-        anchorEl: null
+        exams: []
     }
-    
-    handleClick = (event) => {
-        this.setState({anchorEl: event.currentTarget})
-    };
-    
-    handleClose = () => {
-        this.setState({anchorEl: null})
-    };
+
+    componentDidMount() {
+        
+    }
 
     render(){
         return(
@@ -35,83 +29,110 @@ class ExamList extends React.Component{
                         <TextField style={{width: "100%"}} id="outlined-basic" label="Search exam" variant="outlined" />
                     </form>
                 </div>
-                <Card className="item-exam">
-                    <CardContent className="item-exam-stats">                        
-                        <Typography align="center" variant="h5" component="h2">
-                            89.05
-                        </Typography>
-                        <Typography align="center" className="light-label" color="textSecondary" gutterBottom>
-                            Avarage Score
-                        </Typography>
-                        <Divider/>
-                        <Typography align="center" variant="h5" component="h2">
-                            12
-                        </Typography>
-                        <Typography align="center" className="light-label" color="textSecondary" gutterBottom>
-                            Student Pass
-                        </Typography>
-                    </CardContent>
-                    <CardContent className="item-exam-name">                        
-                        <Typography variant="h5" component="h2">
-                            Ujian Akhir Semester
-                        </Typography>
-                        
-                        <Typography variant="body2" component="p">
-                        Deskripsi materi UAS : 
-                        <br />
-                        {'materi 1'}
-                        <br />
-                        {'materi 1'}
-                        <br />
-                        {'materi 1'}
-                        <br />
-                        {'materi 1'}
-                        </Typography>
-                    </CardContent>
-                    <CardContent className="item-exam-about">
-                        <div style={{display:"flex",flexDirection:"row", justifyContent: "flex-end"}}>
-                            <Typography align="left" className="item-exam-about-problem" variant="body2" component="p">
-                                Total Problem : 10
-                            </Typography>
-                            <div>
-                                <IconButton
-                                    aria-label="more"
-                                    aria-controls="long-menu"
-                                    aria-haspopup="true"
-                                    onClick={this.handleClick}
-                                >
-                                    <MoreVertIcon />
-                                </IconButton>
-                                <Menu
-                                    id="long-menu"
-                                    anchorEl={this.state.anchorEl}
-                                    keepMounted
-                                    open={Boolean(this.state.anchorEl)}
-                                    onClose={this.handleClose}
-                                    PaperProps={{
-                                    style: {
-                                        maxHeight: this.ITEM_HEIGHT * 4.5,
-                                        width: '20ch',
-                                    },
-                                    }}
-                                >
-                                    {['Statistics', 'Review'].map((option) => (
-                                    <MenuItem key={option} selected={option === 'Pyxis'} onClick={this.handleClose}>
-                                        {option}
-                                    </MenuItem>
-                                    ))}
-                                </Menu>
-                            </div>
-                        </div>
-                        
-                        <Typography align="right" className="light-label" color="textSecondary">
-                            Created on 19 Mei 2020
-                        </Typography>
-                    </CardContent>
-                </Card>
+                <List>
+                    {
+                        ['tes','wew','ok'].map((fileName)=>(
+                            <ExamItem key={fileName}></ExamItem>
+                        ))
+                    }
+                </List>
             </div>
         )
     }
 }
 
 export default ExamList;
+
+class ExamItem extends React.Component{
+    ITEM_HEIGHT = 48;
+
+    state = {
+        anchorEl: null
+    }
+    
+    handleClick = (event) => {
+        this.setState({anchorEl: event.currentTarget})
+    };
+    
+    handleClose = () => {
+        this.setState({anchorEl: null})
+    };
+    render() {
+        return (
+            <Card className="item-exam">
+                <CardContent className="item-exam-stats">                        
+                    <Typography align="center" className="text-score">
+                        89.05
+                    </Typography>
+                    <Typography align="center" className="light-label" color="textSecondary" gutterBottom>
+                        Avarage Score
+                    </Typography>
+                    <Divider className="divider"/>
+                    <Typography align="center" className="text-pass">
+                        120
+                    </Typography>
+                    <Typography align="center" className="light-label" color="textSecondary" gutterBottom>
+                        Student Pass
+                    </Typography>
+                </CardContent>
+                <CardContent className="item-exam-name">                        
+                    <Typography className="item-exam-name-title">
+                        Ujian Akhir Semester
+                    </Typography>
+                    
+                    <Typography className="item-exam-name-desc">
+                    Deskripsi materi UAS : 
+                    <br />
+                    {'materi 1'}
+                    <br />
+                    {'materi 1'}
+                    <br />
+                    {'materi 1'}
+                    <br />
+                    {'materi 1'}
+                    </Typography>
+                </CardContent>
+                <CardContent className="item-exam-about">
+                    <div style={{display:"flex",flexDirection:"row", justifyContent: "flex-end"}}>
+                        <Typography align="left" className="item-exam-about-problem" variant="body2" component="p">
+                            Total Problem : 10
+                        </Typography>
+                        <div>
+                            <IconButton
+                                aria-label="more"
+                                aria-controls="long-menu"
+                                aria-haspopup="true"
+                                onClick={this.handleClick}
+                            >
+                                <MoreVertIcon />
+                            </IconButton>
+                            <Menu
+                                id="long-menu"
+                                anchorEl={this.state.anchorEl}
+                                keepMounted
+                                open={Boolean(this.state.anchorEl)}
+                                onClose={this.handleClose}
+                                PaperProps={{
+                                style: {
+                                    maxHeight: this.ITEM_HEIGHT * 4.5,
+                                    width: '20ch',
+                                },
+                                }}
+                            >
+                                {['Statistics', 'Review'].map((option) => (
+                                <MenuItem key={option} selected={option === 'Pyxis'} onClick={this.handleClose}>
+                                    {option}
+                                </MenuItem>
+                                ))}
+                            </Menu>
+                        </div>
+                    </div>
+                    
+                    <Typography align="right" className="light-label" color="textSecondary">
+                        Created on 19 Mei 2020
+                    </Typography>
+                </CardContent>
+            </Card>
+        )
+    }
+}
