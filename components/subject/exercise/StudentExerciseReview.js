48419a65228080023c8aa60683c7fbac72738d4d
Shalahudin Al Ayyub <ayubalandroid@gmail.com>
2021-05-25 09:45:44 +0700

    Add summary review

--- components/subject/exercise/StudentExerciseReview.js
+++ components/subject/exercise/StudentExerciseReview.js
@@ -1,7 +1,8 @@
 
-import { List, ListItem, Button, Typography, Box, IconButton, Container } from "@material-ui/core";
+import { List, ListItem, Button, Typography, Box, IconButton, Container, SvgIcon, Divider } from "@material-ui/core";
 import React from "react";
 import CloseIcon from '@material-ui/icons/Close';
+import Image from "next/image";
 
 class StudentExerciseReview extends React.Component{
     constructor(props){
@@ -21,7 +22,32 @@ class StudentExerciseReview extends React.Component{
                 </Box>
                 <Box className="header-title-container">
                     <Typography className="header-title">Exercise Review</Typography>
-                    <Typography className="header-subtitle">20 October 2020 - 20.25 (30 minutes) </Typography>
+                    <Typography className="header-subtitle">20 October 2020 - 20.25</Typography>
+                </Box>
+                <Box className="review-summary-container">
+                    <Box className="review-summary-item">
+                        <Image className="review-summary-icon" src="/images/timer.svg" height={100} width={100} />
+                        <Typography className="review-summary-value">30 minutes</Typography>
+                        <Typography className="review-summary-label">Duration</Typography>
+                    </Box>
+                    <Divider style={{width: 20, backgroundColor: "white"}}></Divider>
+                    <Box className="review-summary-item">
+                        <Image className="review-summary-icon" src="/images/rank.svg" height={100} width={100} />
+                        <Typography className="review-summary-value">Medium</Typography>
+                        <Typography className="review-summary-label">Difficulty</Typography>
+                    </Box>
+                    <Divider style={{width: 20, backgroundColor: "white"}}></Divider>
+                    <Box className="review-summary-item">
+                        <Image className="review-summary-icon" src="/images/rightanswer.svg" height={100} width={100} />
+                        <Typography className="review-summary-value">80 / 90</Typography>
+                        <Typography className="review-summary-label">RightAnswer</Typography>
+                    </Box>
+                    <Divider style={{width: 20, backgroundColor: "white"}}></Divider>
+                    <Box className="review-summary-item">
+                        <Image className="review-summary-icon" src="/images/fscore.svg" height={100} width={100} />
+                        <Typography className="review-summary-value">88.89</Typography>
+                        <Typography className="review-summary-label">Final Score</Typography>
+                    </Box>
                 </Box>
             </Container>
         )
