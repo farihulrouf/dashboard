891da2e475ed987ed16233a9aa5d93a3d03a8abc
farihul rouf <farihulrouf@gmail.com>
2021-07-17 22:57:28 +0700

    navbar 1.1

--- components/Admin/Navbar.js
+++ components/Admin/Navbar.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
-
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
+import Image from 'next/image'
 import Link from 'next/link'
 import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
 
@@ -27,11 +27,12 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     marginTop:10,
   },
-
+  
   Toolbar: {
     height: 71,
     display: 'flex',
     justifyContent: 'space-between',
+
   },
 
   m_left: {
@@ -83,11 +84,12 @@ export default function Navbar() {
   return (
     <div className={classes.root}>
       
-      <AppBar position="static"
+      <AppBar position="fixed"
         style={{
           height: 71,
           color: 'black',
-          backgroundColor: 'white'
+          backgroundColor: 'white',
+          position: 'fixed'
           }}
       >
         <Toolbar className={classes.Toolbar}>
@@ -95,10 +97,10 @@ export default function Navbar() {
               <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                 <MenuIcon />
               </IconButton>
-              <Link href='/admindashboard'>
+              <Link href='/admin-dashboard'>
               <img
                       alt="klassiq-logo"
-                      src="http://localhost:3003/images/icon.svg"
+                      src="/images/icon.svg"
                       style={
                         {
                           height: 59,
